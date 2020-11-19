bdb7aed07b3e9b287dfbc34d6ae18993ad9034c7
Francis Lafontaine <francis.lafontaine@gmail.com>
2020-11-19 08:05:24 -0500

    Add fake test

---
+++ quote/src/test/java/com/cloud/demo/quote/controller/QuoteControllerTest.java
@@ -0,0 +1,15 @@
+package com.cloud.demo.quote.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class QuoteControllerTest {
+
+    @Test
+    void testSingleSuccessTest() {
+        assertEquals("ABC", "ABC");
+    }
+
+}
